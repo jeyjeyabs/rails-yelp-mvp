7fd86419bfc3bd539f916c2e1a393308b258ecf2
Jeremy Abeille <abeille.jeremy@gmail.com>
2020-08-12 15:49:00 +0200

    restaurant / review models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+sushikev = { name: "Le sushi vert", address: "Rue des mines Saint-Etienne", category: 'japanese', phone_number: '06472567573' }
+jojofrite = { name: "Jojo a la frite", address: "Rue des capucins", category: 'belgian', phone_number: '064725325573' }
+chineseben = { name: "Magic José", address: "Autun", category: 'chinese' }
+matitalia = { name: "Matitalia", address: "Moldavia", category: 'italian', phone_number: '02992955' }
+frenchclem = { name: "Au roi des francs", address: "Lyon", category: 'french', phone_number: '029929959' }
+
+[sushikev, jojofrite, chineseben, matitalia, frenchclem].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
